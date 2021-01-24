510f9079077671a93494ceaa197ef6205dd3dc0a
Ravi <ravichandraraviraviravi@gmail.com>
2021-01-24 21:45:24 +0530

    Added Timer functionality

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,65 @@ export default function App(){
     const [showModal,setShowModal]=useState(false);
     const timerTime=useSelector(state=>state.timer);
     const breakTime=useSelector(state=>state.break);
+    const [timerRunning,startStopTimer]=useState(false);
+    const [clock,setClock]=useState(timerTime);
+    const [breakT,setBreak]=useState(breakTime);
+    const [intervalId,setTntrId]=useState();
+    const [bort,setbort]=useState(false)
 
+    function handleReset(){
+        setClock(timerTime);
+        setBreak(breakTime);
+        setbort(false);
+        startStopTimer(false);
+        clearInterval(intervalId);
+    }
+
+    function handleStartClick(){
+        startStopTimer(true);
+        let timeInSec
+        if(bort){
+            timeInSec=breakT.min*60*1000+breakT.sec*1000;
+        }
+        else{
+
+            timeInSec=clock.min*60*1000+clock.sec*1000;
+        }
+        let endDate=new Date().getTime()+timeInSec;
+        console.log(endDate)
+        let intervalId=setInterval(()=>{
+           console.log(endDate);
+            timeInSec=endDate-new Date().getTime();
+            timeInSec/=1000;
+            console.log(timeInSec);
+            if(bort){
+                setBreak({
+                    min:Math.floor(timeInSec/60),
+                    sec:Math.floor(timeInSec%60)
+                });
+                if(timeInSec<=0){
+                    handleReset();
+                }
+            }
+            else{
+                setClock({
+                    min:Math.floor(timeInSec/60),
+                    sec:Math.floor(timeInSec%60)
+                })
+                if(timeInSec<=0){
+                    setbort(true);
+                }
+            }
+            
+        },1000);
+        setTntrId(intervalId);
+    }
+
+    function handleStopClick(){
+       startStopTimer(false);
+       console.log(intervalId);
+        clearInterval(intervalId);
+    }
     return (
         <div className={styles.app}>
             <h2 className={styles.title}><FontAwesomeIcon icon={faStopwatch} /> &nbsp;Pomotimer</h2>
@@ -20,11 +78,11 @@ export default function App(){
                         <div className={styles.breakLabel}>Break</div>
                     </div>
                     <div className={styles.clock}>
-                        <div className={styles.time}><span className={styles.minutes}>12</span><span className={styles.colon}>:</span><span className={styles.seconds}>00</span></div>
+                        <div className={styles.time}><span className={styles.minutes}>{clock.min}</span><span className={styles.colon}>:</span><span className={styles.seconds}>{clock.sec}</span></div>
                     </div>
                     <div className={styles.controls}>
-                        <button className='btn btn-primary'>Start</button>
-                        <button className='btn btn-primary'>Restart</button>
+                        {(timerRunning)?<button className='btn btn-primary' onClick={handleStopClick}>Pause</button>:<button className='btn btn-primary' onClick={handleStartClick}>Start</button>}
+                        <button className='btn btn-primary' onClick={handleReset}>Restart</button>
                     </div>
               </div>
                
